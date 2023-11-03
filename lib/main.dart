96ea66d64d92032f1dd3cf65929a9be80049c638
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-11-03 13:55:48 +0000

    Add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_verkefni_1/GradientBackgroundContainer.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: AppStyle(Colors.green, Colors.greenAccent),
+      ),
+    ),
+  );
+}
